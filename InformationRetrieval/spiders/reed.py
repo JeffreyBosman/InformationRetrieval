30fbcf687d0abbb5afb43bc829e6c80efb0add47
Jeffrey Bosman <jeffrey_bosman@hotmail.nl>
2019-02-12 11:02:06 +0100

    initial

---
+++ InformationRetrieval/spiders/reed.py
@@ -0,0 +1,42 @@
+
+import scrapy
+class JobsSpider(scrapy.Spider):
+    name = "reed"
+    download_delay = 1
+    user_agent = 'University of Amsterdam - Information Retrieval Project  (persenal_email@gmail.com)' 
+    httpcache_enabled = False 
+    start_urls = [
+        'https://www.reed.co.uk/jobs',
+		]
+
+    def parse(self, response):
+        # follow links to author pages
+        for href in response.css("article.job-result a::attr(href)").getall():
+            if href.find("source") > 0:
+                yield response.follow(href, self.parse_vacancie)
+            else:
+                print('Skip this page')
+				
+        # follow pagination links
+        next_page = response.css("[id=nextPage]::attr(href)").get()
+#        next_page = response.css("#nextPage ::attr(href)")
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+			
+			
+    def parse_vacancie(self, response):
+        list_disc = response.css("div.description *::text").getall()
+        title_obj = response.css('div.col-xs-12 h1::text').get()
+        company = response.css("div.posted span::text").get()
+        location =response.css("div.location.col-xs-12.col-sm-6.col-md-6.col-lg-6 ::text").getall()
+
+        yield {
+            'title': title_obj,
+            'company': company,
+            'description': list_disc,
+            'location': location,
+			'URL': response.request.url,
+        }
+
+
